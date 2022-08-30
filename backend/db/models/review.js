5775c932dbe3b1530c6a34457e0e61193c1a20af
Thenu <anbunachiappan@gmail.com>
2022-08-30 14:10:36 -0700

    typo

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,27 +11,27 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.belongsTo(models.User,{foreignKey:"userId"}),
-      Review.belongsTo(models.Spot,{foreignKey:"spotId"}),
-      Review.hasMany(models.ReviewImage,{foreignKey:"reviewId"})
+      Review.belongsTo(models.User, { foreignKey: "userId" }),
+        Review.belongsTo(models.Spot, { foreignKey: "spotId" }),
+        Review.hasMany(models.ReviewImage, { foreignKey: "reviewId" })
     }
   }
   Review.init({
     spotId: {
-      type:DataTypes.INTEGER,
-      allowNull:false
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
-    userId:{
-      type:DataTypes.INTEGER,
-      allowNull:false
-    },git 
-    review:{
-      type:DataTypes.STRING,
-      allowNull:false
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
-    stars:{
-      type:DataTypes.INTEGER,
-      allowNull:false
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
 
   }, {
