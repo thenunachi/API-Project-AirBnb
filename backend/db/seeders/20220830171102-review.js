0b4a8b64b9b8bcd753aefd526c502e3200e75c8d
Thenu <anbunachiappan@gmail.com>
2022-09-22 00:40:21 -0700

    Mistakely gave stars value in decimal corrected it

--- backend/db/seeders/20220830171102-review.js
+++ backend/db/seeders/20220830171102-review.js
@@ -30,6 +30,128 @@ module.exports = {
         review: "Medium",
         stars: 3
       },
+      {
+        spotId: 3,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 4,
+        userId: 2,
+        review: "Medium",
+        stars: 5
+      },
+      {
+        spotId: 5,
+        userId: 1,
+        review: "Medium",
+        stars: 4
+      },
+      {
+        spotId: 6,
+        userId: 2,
+        review: "Medium",
+        stars: 5
+      },
+      {
+        spotId: 7,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 8,
+        userId: 1,
+        review: "Medium",
+        stars: 2
+      },
+      {
+        spotId: 9,
+        userId: 2,
+        review: "Medium",
+        stars: 4
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 10,
+        userId: 1,
+        review: "Medium",
+        stars: 5
+      },
+      {
+        spotId: 11,
+        userId: 3,
+        review: "Medium",
+        stars: 4
+      },
+      {
+        spotId: 12,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 13,
+        userId: 2,
+        review: "Medium",
+        stars: 5
+      },
+      {
+        spotId: 14,
+        userId: 2,
+        review: "Medium",
+        stars: 5
+      },
+      {
+        spotId: 15,
+        userId: 1,
+        review: "Medium",
+        stars: 3
+      },
+
+      {
+        spotId: 16,
+        userId: 1,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 17,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 18,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 18,
+        userId: 3,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 19,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      },
+      {
+        spotId: 20,
+        userId: 2,
+        review: "Medium",
+        stars: 3
+      }
+
     ], {});
   },
 
