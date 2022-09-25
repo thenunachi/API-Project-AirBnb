faa3f3fc9ec358e93077858bb6bd22c29cb65b01
Thenu <anbunachiappan@gmail.com>
2022-09-25 12:49:09 -0700

    Changed seed file

--- backend/db/seeders/20220830171053-spot.js
+++ backend/db/seeders/20220830171053-spot.js
@@ -21,8 +21,8 @@ module.exports = {
          lat:69.21,
          lng:42.43,
          name:"Thenu",
-         description:"Welcome to this beautiful house located in Granite Falls.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
-         price:386.21
+          description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
+         price:100.21
        },
        {
         ownerId:2,
@@ -33,8 +33,8 @@ module.exports = {
         lat:79.21,
         lng:52.43,
         name:"Kalyan",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
-        price:625.21
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
+        price:150.21
       },
       {
         ownerId:3,
@@ -45,7 +45,7 @@ module.exports = {
         lat:72.21,
         lng:51.43,
         name:"John",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:57.44
       },
       {
@@ -57,7 +57,7 @@ module.exports = {
         lat:80.21,
         lng:51.43,
         name:"Smith",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:48.21
       },
       {
@@ -69,7 +69,7 @@ module.exports = {
         lat:89.21,
         lng:82.43,
         name:"Ted",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:58.21
       },
       {
@@ -81,7 +81,7 @@ module.exports = {
         lat:89.21,
         lng:32.43,
         name:"Phill",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:98.21
       },
       {
@@ -93,7 +93,7 @@ module.exports = {
         lat:99.21,
         lng:62.43,
         name:"Naren",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:98.21
       },
       {
@@ -105,7 +105,7 @@ module.exports = {
         lat:89.21,
         lng:62.43,
         name:"Ram",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:58.21
       },
       {
@@ -117,7 +117,7 @@ module.exports = {
         lat:89.21,
         lng:62.43,
         name:"Karthik",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:98.21
       },
       {
@@ -129,7 +129,7 @@ module.exports = {
         lat:78.21,
         lng:56.43,
         name:"Dhaniya",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:102.21
       },
       {
@@ -141,7 +141,7 @@ module.exports = {
         lat:89.21,
         lng:59.43,
         name:"Bindhu",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:88.21
       },
       {
@@ -153,7 +153,7 @@ module.exports = {
         lat:69.21,
         lng:92.43,
         name:"Ruku",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:78.21
       },
       {
@@ -165,7 +165,7 @@ module.exports = {
         lat:99.21,
         lng:62.43,
         name:"Diya",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:98.21
       },
       {
@@ -177,8 +177,8 @@ module.exports = {
         lat:99.21,
         lng:52.43,
         name:"Prasad",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
-        price:508.21
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
+        price:120.21
       },
       {
         ownerId:3,
@@ -189,7 +189,7 @@ module.exports = {
         lat:59.21,
         lng:572.43,
         name:"Millie",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.e",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:98.21
       },
       {
@@ -201,7 +201,7 @@ module.exports = {
         lat:89.21,
         lng:62.43,
         name:"Saranya",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:88.21
       },
       {
@@ -213,7 +213,7 @@ module.exports = {
         lat:79.21,
         lng:52.43,
         name:"Chase",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:89.21
       },
       {
@@ -225,7 +225,7 @@ module.exports = {
         lat:79.21,
         lng:52.43,
         name:"Foreman",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:45.21
       },
       {
@@ -237,7 +237,7 @@ module.exports = {
         lat:79.21,
         lng:52.43,
         name:"House",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:98.21
       },
       {
@@ -249,7 +249,7 @@ module.exports = {
         lat:79.21,
         lng:52.43,
         name:"Cameron",
-        description:"Welcome to this beautiful house located in LeavenWorth.Modern newly constructed house with an open floor plan and large windows overlooking the Wenatchee river. The kitchen comes fully equipped with all appliances, dishware and utensils. There is a large screen TV with comfortable seating for 6 as well as a large dining room table. The house has an en suite king bed master bedroom.. attached bath and a large shower overlooking the river. There are two guest bedrooms each having a beautiful view of the river.",
+        description:"Welcome to this beautiful house located in Granite Falls. The kitchen comes fully equipped with all appliances, dishware and utensils.The house has an suite king bed master bedroom attached bath and a large shower overlooking the river.",
         price:89.21
       }
       
