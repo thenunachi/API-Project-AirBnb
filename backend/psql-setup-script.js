0b4fa74a4753d84bd5cf1bf8711ede8c5d239947
Thenu <anbunachiappan@gmail.com>
2023-10-13 11:45:09 -0700

    Deleted node modules and package.lock.json

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,8 +1,8 @@
 const { sequelize } = require('./db/models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
-  console.log(data,"DATA FROM SQL")
-  console.log(process.env.SCHEMA,"schema")
+  // console.log(data,"DATA FROM SQL")
+  // console.log(process.env.SCHEMA,"schema")
   if (!data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA);
   }
