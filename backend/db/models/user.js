c2236cdb98723c211664c694fde7a5effab724b4
Thenu <anbunachiappan@gmail.com>
2023-10-16 14:01:16 -0700

    Changed user. modal file by adding schema to it

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -95,6 +95,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
+      schema: process.env.SCHEMA,
       modelName: "User",
       defaultScope: {
         attributes: {
